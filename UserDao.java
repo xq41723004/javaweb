cd733282dd923d3f59fa6842669987951f179bd9
肖强 <37110707+xq41723004@users.noreply.github.com>
2018-06-07 10:46:52 +0800

    数据库

---
+++ UserDao.java
@@ -0,0 +1,76 @@
+﻿package com.wzc.loginDao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+	public String findUser(String username){
+		String psw = null;
+		String sql = "select * from user where username=?";
+		Connection con =getConnection();
+		PreparedStatement pstmt =null;
+		ResultSet rs = null;
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, username);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				psw=rs.getString("password");
+			}else{
+				psw=null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(con!=null)con.close();
+			} catch (SQLException e) {		
+				e.printStackTrace();
+			}
+		}
+		return psw;
+	}
+	public void addUser(String username,String psw){
+		Connection con = getConnection();
+		PreparedStatement pstmt =null;
+		String sql = "INSERT INTO user(username,password) VALUES(?,?)";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, username);
+			pstmt.setString(2, psw);
+			pstmt.executeUpdate();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(con!=null)con.close();
+			}catch (SQLException e) {	
+				e.printStackTrace();
+			}
+		}
+	}
+	public static Connection getConnection(){
+		String driver ="com.mysql.jdbc.Driver";
+		String url ="jdbc:mysql://localhost:3306/student";
+		String user ="root";
+		String password ="ROOT";
+		Connection connection =null;
+		try {
+			Class.forName(driver);
+			connection =DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	public static void main(String[] args) {
+//		System.out.println(new UserDao().findUser("123"));
+//		new UserDao().addUser("1345", "1345");
+	}
