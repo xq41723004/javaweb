aeff40c75530f19b23697e2f9abccdee0ae46c45
肖强 <37110707+xq41723004@users.noreply.github.com>
2018-06-07 11:10:04 +0800

    二维码生成

---
+++ VerifyCodeServlet.java
@@ -0,0 +1,26 @@
+package com.wzc.loginServlet;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wzc.utils.VerifyCode;
+
+
+public class VerifyCodeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		VerifyCode vc = new VerifyCode();
+		BufferedImage image = vc.getImage(90,30);
+		request.getSession().setAttribute("sessionverify", vc.getText());
+		VerifyCode.output(image, response.getOutputStream());
+
+	}
+
+}
